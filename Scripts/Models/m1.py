3e3c7ab516fe52e1970a42a427eee17c9563867f
trey <treyoehmler@gmail.com>
2019-04-15 18:10:19 -0400

    updated training script

---
+++ Scripts/Models/m1.py
@@ -0,0 +1,35 @@
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, BatchNormalization
+from keras.layers import Conv2D, MaxPooling2D
+from keras.regularizers import l1_l2
+from keras.optimizers import SGD
+
+def compile():
+    single = Sequential()
+
+    single.add(Conv2D(64, (7,7), strides=(1,1), padding='valid', activation='relu', input_shape=(33,33,4)))
+    single.add(BatchNormalization())
+    single.add(Dropout(0.5))
+
+    single.add(Conv2D(128, (5,5), strides=(1,1), padding='valid', activation='relu'))
+    single.add(BatchNormalization())
+    single.add(Dropout(0.5))
+
+    single.add(Conv2D(128, (5,5), strides=(1,1), padding='valid', activation='relu'))
+    single.add(BatchNormalization())
+    single.add(Dropout(0.5))
+
+    single.add(Conv2D(128, (3,3), strides=(1,1), padding='valid', activation='relu'))
+    single.add(Dropout(0.25))
+
+    single.add(Flatten())
+    single.add(Dense(5, activation='softmax'))
+
+    sgd = SGD(lr=0.001, decay=0.01, momentum=0.9)
+    single.compile(loss='categorical_crossentropy', optimizer='sgd')
+
+    return single
+
+
+
+ 
--- Scripts/train.py
+++ Scripts/train.py
@@ -3,14 +3,34 @@ from glob import glob
 import numpy as np
 from Utils import config
 from Utils import patches
+from Models import m1
+from keras.utils import np_utils
+import random
 
 root = '/Users/treyoehmler/dev/tumors/seg/'
 data_root = config.processed_root + 'data/'
 print(data_root)
 
-training_patches = patches.generate_train(1, config.train_root, 33)
-print(training_patches[0].shape)
-print(training_patches[1].shape)
+
+training_patches = patches.generate_train(10000, config.train_root, 33)
+
+patches = training_patches[0]
+labels = np_utils.to_categorical(training_patches[1])
+
+shuffle = list(zip(patches, labels))
+np.random.shuffle(shuffle)
+X, Y = zip(*shuffle)
+
+x_train = np.array(X)
+y_train = np.array(Y)
+
+model = m1.compile()
+model.fit(x_train, y_train, batch_size=128, epochs=7, validation_split=0.1, verbose=1) 
+
+model.save('/home/trey/seg/Outputs/Models/m1.h5')
+
+
+
 
 
 # load compiled model
