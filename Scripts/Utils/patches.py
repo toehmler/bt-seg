2862dfd58430e0ddfe9112eb066666929de94e22
trey <treyoehmler@gmail.com>
2019-04-15 17:25:26 -0400

    quick fix

--- Scripts/Utils/patches.py
+++ Scripts/Utils/patches.py
@@ -43,6 +43,7 @@ def generate_train(num, root, size):
             label_img = Image.open(root + 'labels/train/' + slice_path)
             slice_np = np.asarray(label_img)
 
+            slice_label = slice_np.copy()
             if np.max(slice_np) != 0:
                 slice_label /= (np.max(slice_np) - np.min(slice_np))
             
