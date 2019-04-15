be7270dc5281129fd868cb2a3935aa2d0df1a38f
trey <treyoehmler@gmail.com>
2019-04-15 16:46:22 -0400

    quick fix

--- Scripts/Utils/config.py
+++ Scripts/Utils/config.py
@@ -4,7 +4,7 @@ root  = '/home/trey/data/BRATS2015_Training/HGG'
 processed_root = '/Users/treyoehmler/dev/tumors/data/processed/'
 
 #train_root = '/Users/treyoehmler/dev/tumors/data/processed/'
-train_root = '/home/trey/data/processed/train/'
+train_root = '/home/trey/data/processed/'
 #test_root = '/home/trey/data/processed/test/'
 
 
