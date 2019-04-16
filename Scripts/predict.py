40579c3795ce8c91a757efc436b1cf4fe6885037
trey <treyoehmler@gmail.com>
2019-04-15 22:34:53 -0400

    quick fix

--- Scripts/predict.py
+++ Scripts/predict.py
@@ -10,8 +10,13 @@ from Utils import config
 
 data_img = Image.open(config.train_root + 'data/test/pat200_50_data.png')
 test_data = np.asarray(data_img)
+test_data = test_data.reshape(4, 240, 240)
 
-test_patches = extract_patches_2d(test_data, (33,33))
+data = np.zeros((240, 240, 4))
+for i in range(4):
+    data[:,:,i] = test_data[i,:,:]
+
+test_patches = extract_patches_2d(data, (33,33))
 
 model = load_model('/home/trey/bt-seg/Outputs/Models/Trained/m1.h5')
 
