f8adb9ec156a1d1988d1fef6690e573c36ba3db3
trey <treyoehmler@gmail.com>
2019-04-16 20:48:11 -0400

    quick fix

--- Scripts/patch_test.py
+++ Scripts/patch_test.py
@@ -12,11 +12,13 @@ for i, patch in enumerate(patches):
     strip = np.zeros((4, 33, 33))
     for j in range(4):
         strip[j:,:] = patch[:,:,j]
-
+    
     strip_img = strip.reshape(132, 33)
+    print(np.min(strip_img))
+    print(np.max(strip_img))
     print(strip_img.shape)
-    strip_img = strip.astype(np.uint8)
-    imageio.imwrite(out_path + '{}_patch.png'.format(i), strip_img)
+#    strip_img = strip.astype(np.uint8)
+#    imageio.imwrite(out_path + '{}_patch.png'.format(i), strip_img)
 
 # reshape patches to strips
 
