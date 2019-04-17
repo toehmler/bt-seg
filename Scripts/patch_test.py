4608d418ce6e80238a88b0834a0d86d4c3c3e862
trey <treyoehmler@gmail.com>
2019-04-16 21:35:24 -0400

    quick fix

--- Scripts/patch_test.py
+++ Scripts/patch_test.py
@@ -8,12 +8,14 @@ import imageio
 Description of tests:
     - test1: no restriction placed on len(np.argwhere(patch == 0))
     - test2: resample if len(np.argwhere(patch == 0)) > (33*33)
+    - test3: resample if len(np.argwhere(patch == 0)) > ((33*33) / 4)
+
 '''
 
 
 patches_per = 100
 
-out_path = '/home/trey/bt-seg/Outputs/Patches/test2/'
+out_path = '/home/trey/bt-seg/Outputs/Patches/test3/'
 test_patches = patches.generate_train(patches_per, config.train_root, 33)
 
 patches = test_patches[0]
@@ -26,4 +28,5 @@ for i in range(len(test_patches[0])):
         strip[j,:,:] = patch[:,:,j]
     
     strip_img = strip.reshape(132,33)
-    imageio.imwrite(out_path + 'label_{}_patch_{}.png'.format(label, i), strip_img)
+    s = strip_img.astype(np.uint8)
+    imageio.imwrite(out_path + 'label_{}_patch_{}.png'.format(label, i), s)
