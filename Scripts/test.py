4c8efaa682b43d025d658308e4eb9ef3e5a5261f
trey <treyoehmler@gmail.com>
2019-04-18 00:33:24 -0400

    quick fix

--- Scripts/test.py
+++ Scripts/test.py
@@ -2,53 +2,66 @@ import numpy as np
 import imageio
 from PIL import Image
 from sklearn.metrics import classification_report, precision_score, recall_score
+import sys
 
 ''' 
 Command line arguments:
     - path to data
+    - name of model (eg m1_4)
     - patient no  (for prediction)
     - slice no (for prediction)
+'''
 
 # -*- coding: utf-8 -*-
 """test.py 
 """
 
-root = '/Users/treyoehmler/dev/tumors/seg/'
+data_path = sys.argv[1]
+model_name = sys.argv[2]
+patient_no = sys.argv[3]
+slice_no = sys.argv[4]
 
-prediction = np.load(root + 'Outputs/210_60_m3.npy')
-pred = prediction.reshape(208, 208)
-p = np.pad(pred, (16, 16), mode='edge')
+model = load_model('Models/Outputs/Trained/' + model_name + '.h5')
 
-label_img = Image.open(root  + 'pat210_60_label.png')
-label = np.asarray(label_img)
+full_patch = data_path + 'data/test/pat' + patient_no + '_' + slice_no + '_data.png'
+data_img = Image.open(full_path)
+test_data = np.asarray(data_img)
+test_data = reshape(4, 240, 240)
+input_data = np.zeros((240, 240, 4))
+for i in range(4):
+    input_data[:,:,i] = test_data[i,:,:]
+
+test_patches = extract_patches_2d(input_data, (33,33))
+prediction = model.predict_classes(test_patches)
+np.save('Outputs/Predictions/{}_{}_{}.npy'.format(model_name, patient_no, slice_no))
+
+pred_sq = prediction_reshape(208,208)
+p = np.pad(pred_sq, (16, 16), mode='edge')
 
 ones = np.argwhere(p == 1)
 twos = np.argwhere(p == 2)
 threes = np.argwhere(p == 3)
 fours = np.argwhere(p == 4)
 
-print(len(ones))
-print(len(twos))
-print(len(threes))
-print(len(fours))
 
 new = p.copy() 
 for i in range(len(ones)):
-    new[ones[i][0]][ones[i][1]] = 255 
+    new[ones[i][0]][ones[i][1]] = 63 
 for i in range(len(twos)):
-    new[twos[i][0]][twos[i][1]] = 191
+    new[twos[i][0]][twos[i][1]] = 127
 for i in range(len(threes)):
-    new[threes[i][0]][threes[i][1]] = 127
+    new[threes[i][0]][threes[i][1]] = 191 
 for i in range(len(fours)):
-    new[fours[i][0]][fours[i][1]] = 63
+    new[fours[i][0]][fours[i][1]] = 255
 
 new_img = new.astype(np.uint8)
-imageio.imwrite('210_60_m3.png', new_img)
+imageio.imwrite('Outputs/Segmentations/{}_{}_{}.png'.format(model_name, patient_no), new_img)
 
+'''
 y = label[15:223, 15:223]
 truth = y.reshape(43264,)
 print(classification_report(truth, prediction, labels=[0,1,2,3,4]))
-
+'''
 
 
 
