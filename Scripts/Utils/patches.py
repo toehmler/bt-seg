cabadc5e5344068211ff94ff284391e3658feef3
trey <treyoehmler@gmail.com>
2019-04-17 23:18:57 -0400

    quick fix

--- Scripts/Utils/patches.py
+++ Scripts/Utils/patches.py
@@ -50,15 +50,16 @@ def generate_train(num, root, size):
             slice_data = np.asarray(slice_img)
             slice_data = slice_data.reshape(4, 240, 240)
             p = slice_data[:,bounds[0]:bounds[1], bounds[2]:bounds[3]]
+
             if p.shape != (4, size, size):
+                continue
+
+            if len(np.argwhere(patch == 0)) > (33 * 33): 
                 continue
 
             patch = np.zeros((size,size,4))
             for j in range(4):
                 patch[:,:,j] = p[j,:,:]
-
-            if len(np.argwhere(patch == 0)) > (33 * 33): 
-                continue
 
             patches.append(patch)
             labels.append(i)
