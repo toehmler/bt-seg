02617fdb06b9ef8f1b363b6294de304497b79b32
trey <treyoehmler@gmail.com>
2019-04-24 23:22:50 -0400

    quick fix

--- Scripts/n4.py
+++ Scripts/n4.py
@@ -16,7 +16,7 @@ Usage:  n4.py
 
 config = configparser.ConfigParser()
 config.read('config.ini')
-root = config['DEFAULT']['brats']
+root = config.get('default', 'brats')
 paths = os.listdir(root)
 paths = [os.path.join(root, name) for name in paths if 'pat' in name.lower()]
 
