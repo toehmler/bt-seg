0af020fd697f9f92ebd2666254ae2037d9049013
trey <treyoehmler@gmail.com>
2019-04-29 16:00:41 -0400

    quick fix

--- Scripts/Utils/patient.py
+++ Scripts/Utils/patient.py
@@ -1,3 +1,5 @@
+# Author: Trey Oehmler
+
 import SimpleITK as sitk
 from glob import glob
 import subprocess
@@ -5,15 +7,17 @@ import numpy as np
 import os
 from skimage import io
 from nipype.interfaces.ants.segmentation import N4BiasFieldCorrection
-
-# -*- coding: utf-8 -*-
+from tqdm import tqdm 
+'''
+==================================================
+NOT USING
+--------------------------------------------------
 
 def apply_n4(path):
     t1 = glob(path + '/*T1.*/*.mha')
     t1c = glob(path + '/*T1c*/*.mha')
     n4_bfc(t1[0])
     n4_bfc(t1c[0])
-
 
 def n4_bfc(input_path):
     print("Applying bias correction...")
@@ -26,25 +30,13 @@ def n4_bfc(input_path):
     n4.inputs.n_iterations = [50, 50, 30, 20]
     n4.inputs.output_image = input_path.replace('.mha', '_n4.mha')
     n4.run()
-
+==================================================
 '''
-def n4_correction(im_input):
-    n4 = N4BiasFieldCorrection()
-    n4.inputs.dimension = 3
-    n4.inputs.input_image = im_input
-    n4.inputs.bspline_fitting_distance = 300
-    n4.inputs.shrink_factor = 3
-    n4.inputs.n_iterations = [50, 50, 30, 20]
-    n4.inputs.output_image = im_input.replace('.nii.gz', '_corrected.nii.gz')
-    n4.run()
 
-'''
-# updating to look for n4 suffixed scans
 def load_scans(path):
-    """
-    scans = [flair, t1, t1c, t2, gt]
-    output shape: (155, 5, 240, 240)
-    """
+    '''
+    output shape: (5, 155, 240, 240)
+    '''
     flair = glob(path + '/*Flair*/*.mha')
     t1 = glob(path + '/*T1.*/*.mha')
     t1c = glob(path + '/*T1c.*/*.mha')
@@ -53,37 +45,36 @@ def load_scans(path):
 
     mod_paths = [flair[0], t1[0], t1c[0], t2[0], gt[0]]
     mods = []
-    for mod_path in mod_paths:
-        mod_img = io.imread(mod_path, plugin='simpleitk').astype('float')
+    for i in tqdm(range(5)):
+        mod_path = mod_paths[i]
+        mod_img = io.imread(mod_path, plugin='simpleitk').astype(np.float32)
         mod_array = np.array(mod_img)
         mods.append(mod_array)
 
     data_mods = np.array(mods)
-    data_slices = np.zeros((155, 5, 240, 240))
-    for i in range(155):
-        for j in range(5):
-            data_slices[i,j,:,:] = data_mods[j][i,:,:]
-
-    return data_slices
+    return data_mods
 
 
 def normalize(scans):
-    normed_scans = np.zeros((155, 5, 240, 240))
-    #exclude gt from normalization
-    gt = scans[:,4,:,:]
-    for i in range(155):
-        for j in range(4):
-            normed_slice = norm_slice(scans[i][j,:,:])
-            normed_scans[i,j,:,:] = normed_slice
-    normed_scans[:,4,:,:] = gt
+    '''
+    output shape: (155, 240, 240, 5)
+    '''
+    normed_scans = np.zeros((155, 240, 240, 5))
+    # exclude ground truth
+    normed_scans[:,:,:,4] = scans[4] 
+    for i in range(4):
+        norm_mod = normalize_mod(scans[i])
+        normed_scans[:,:,:,i] = norm_mod
     return normed_scans
 
-    
-def norm_slice(slice):
-    b, t = np.percentile(slice, (0.5,99.5))
-    slice = np.clip(slice, b, t)
-    if np.std(slice) == 0:
-        return slice
-    else:
-        return (slice - np.mean(slice)) / np.std(slice)
-    
+
+def normalize_mod(mod):
+    x_start = mod.shape[0] // 4
+    x_range = mod.shape[0] // 2
+    y_start = mod.shape[1] // 4
+    y_range = mod.shape[1] // 2
+    z_start = mod.shape[2] // 4
+    z_range = mod.shape[2] // 2
+    roi = mod[x_start:x_start+x_range, y_start:y_start+y_range, z_start:z_start+z_range]
+    return (mod - np.min(roi)) / np.std(roi)
+   
--- Scripts/preprocess.py
+++ Scripts/preprocess.py
@@ -1,3 +1,4 @@
+# Author: Trey Oehmler
 from Utils import patient
 import imageio
 import numpy as np
@@ -7,8 +8,19 @@ from tqdm import tqdm
 import json
 import scipy.misc
 from PIL import Image
+from skimage import io
 
+'''
+==================================================
+preprocess.py 
+--------------------------------------------------
+(1) Loads scans each patient (.mha files)
+(2) Normalizes pixel itensities 
+(3) Splits patients into training and test sets
+(4) Saves np.array (float32) for each patient
+==================================================
 
+'''
 with open('config.json') as config_file:
     config = json.load(config_file)
 
@@ -17,25 +29,14 @@ paths = os.listdir(root)
 paths = [os.path.join(root, name) for name in paths if 'pat' in name.lower()]
 out_path = config['processed']
 
-for patient_no, path in enumerate(paths):
-    print("Processing patient " + str(patient_no))
-    scans = patient.load_scans(path)
-    normed_scans = patient.normalize(scans) # (155, 5, 240, 240)
-    for slice_no, slice in enumerate(normed_scans):
-        strip = slice[:4,:,:]
-        label = slice[4,:,:]
-        
-        if np.max(strip) != 0:
-            strip /= np.max(strip)
 
-        if np.min(strip) <= -1:
-            strip /= abs(np.min(strip))
-        
-        strip = strip.reshape(960,240)
-        label = label.astype(np.uint8)
-        if patient_no < 190:
-            imageio.imwrite(out_path+'/train/pat{}_{}_data.png'.format(patient_no, slice_no), strip)
-            imageio.imwrite(out_path+'/train/pat{}_{}_label.png'.format(patient_no, slice_no), label)
-        else:
-            imageio.imwrite(out_path+'/test/pat{}_{}_data.png'.format(patient_no, slice_no), strip)
-            imageio.imwrite(out_path+'/test/pat{}_{}_label.png'.format(patient_no, slice_no), label)
+for i in tqdm(range(len(paths))):
+    scans = patient.load_scans(paths[i]) # (5, 155, 240, 240)
+    patient_data = patient.normalize(scans) # (155, 240, 240, 5)
+    if i < 190:
+        np.save(out_path + '/train/pat_{}.npy'.format(i), patient_data)
+    else:
+        np.save(out_path + '/test/pat_{}.npy'.format(i), patient_data)
+
+
+
