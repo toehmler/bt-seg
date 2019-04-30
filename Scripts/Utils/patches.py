335636e5b718ed3c37a3e70f4c56d37f9ac19136
trey <treyoehmler@gmail.com>
2019-04-29 22:08:44 -0400

    quick fix

--- Scripts/Utils/patches.py
+++ Scripts/Utils/patches.py
@@ -62,8 +62,8 @@ def generate_train(num, num_per_class, root, size):
                 patches.append(patch)
                 labels.append(class_label)
                 class_label += 1
-        del data
         data.close()
+        del data
     labels = np.array(labels).astype(np.float16)
     labels = np_utils.to_categorical(labels)
     patches = np.array(patches)
