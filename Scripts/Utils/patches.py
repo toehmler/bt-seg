1a776b56d19cac7ba0318a8aa504ca469134ddfb
trey <treyoehmler@gmail.com>
2019-05-01 11:06:49 -0400

    quick fix

--- Scripts/Utils/patches.py
+++ Scripts/Utils/patches.py
@@ -71,7 +71,7 @@ def generate_class_patches(path, num, size, class_num):
 
     data_addr = id(data)
     pat_addr = id(patient)
-    del data
+    del patient
     print('data ref count:')
     print(PyObject.from_address(data_addr).refcnt)
     print('patient ref count:')
