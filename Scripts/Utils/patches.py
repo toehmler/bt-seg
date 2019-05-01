f4509c337e6a1793d493a360f5c108f1d7df534f
trey <treyoehmler@gmail.com>
2019-05-01 11:39:21 -0400

    quick fix

--- Scripts/Utils/patches.py
+++ Scripts/Utils/patches.py
@@ -84,7 +84,7 @@ def generate_class_patches(path, num, size, class_num):
 def generate_patient_patches(path, num_per, size):
     patches = np.zeros((5, num_per, size, size, 4)).astype(np.float32)
     labels = np.zeros((5, num_per))
-    for i in range(5):
+    for i in tqdm(range(5)):
         class_patches = generate_class_patches(path, num_per, size, i)
         patches[i] = class_patches[0]
         labels[i] = class_patches[1]
@@ -101,7 +101,7 @@ def generate_train_batch(root, num_per, size, start, num_patients):
     patches = np.zeros((num_patients,5*num_per,size,size,4)).astype(np.float32)
     labels = np.zeros((num_patients,5*num_per,5)).astype(np.float32)
 
-    for i in range(num_patients):
+    for i in tqdm(range(num_patients)):
         path = '{}/train/pat_{}.npz'.format(root, start + i)
         patient = generate_patient_patches(path, num_per, size)
         patches[i] = patient[0]
@@ -128,7 +128,7 @@ def batch_wrapper(root):
         y = np.array(y)
         train_x[i] = x
         train_y[i] = y # CHECK SHAPE OF LABELS
-    train_x = train_x.reshape(num_batches*2*5*50, 33, 33, 4)
+    train_x = train_x.reshape(num_batches*5*5*50, 33, 33, 4)
     print(train_x.shape)
     print(train_y.shape)
 
