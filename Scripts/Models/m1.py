d77e8ddbca9cce7020d8a4cca1cc60a7a5fd3231
trey <treyoehmler@gmail.com>
2019-05-01 21:12:31 -0400

    quick fix

--- Scripts/Models/m1.py
+++ Scripts/Models/m1.py
@@ -8,7 +8,7 @@ def compile():
     single = Sequential()
 
     single.add(Conv2D(64, (7,7), 
-                      strides=(1,1), padding='valid', activation='relu'
+                      strides=(1,1), padding='valid', activation='relu',
                       kernel_regularizer=l1_l2(l1=0.01, l2=0.01), 
                       input_shape=(33,33,4)))
 
