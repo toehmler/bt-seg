d71c12d9081bf47d541570f96b535e1a8101ebd5
trey <treyoehmler@gmail.com>
2019-05-01 20:43:42 -0400

    quick fix

--- Scripts/Models/m1.py
+++ Scripts/Models/m1.py
@@ -7,19 +7,29 @@ from keras.regularizers import l1_l2
 def compile():
     single = Sequential()
 
-    single.add(Conv2D(64, (7,7), strides=(1,1), padding='valid', activation='relu', input_shape=(33,33,4)))
+    single.add(Conv2D(64, (7,7), 
+                      strides=(1,1), padding='valid', activation='relu'
+                      kernel_regularizer=l1_l2(l1=0.01, l2=0.01), 
+                      input_shape=(33,33,4)))
+
     single.add(BatchNormalization())
     single.add(Dropout(0.5))
 
-    single.add(Conv2D(128, (5,5), strides=(1,1), padding='valid', activation='relu'))
+    single.add(Conv2D(128, (5,5), strides=(1,1), padding='valid', 
+                      kernel_regularizer=l1_l2(l1=0.01, l2=0.01),
+                      activation='relu'))
     single.add(BatchNormalization())
     single.add(Dropout(0.5))
 
-    single.add(Conv2D(128, (5,5), strides=(1,1), padding='valid', activation='relu'))
+    single.add(Conv2D(128, (5,5), strides=(1,1), padding='valid', 
+                      kernel_regularizer=l1_l2(l1=0.01, l2=0.01),
+                      activation='relu'))
     single.add(BatchNormalization())
     single.add(Dropout(0.5))
 
-    single.add(Conv2D(128, (3,3), strides=(1,1), padding='valid', activation='relu'))
+    single.add(Conv2D(128, (3,3), strides=(1,1), padding='valid', 
+                      kernel_regularizer=l1_l2(l1=0.01, l2=0.01),
+                      activation='relu'))
     single.add(Dropout(0.25))
 
     single.add(Flatten())
