482a8106b93ecab5d049e144d14bf010a9ffe3b7
trey <treyoehmler@gmail.com>
2019-05-05 23:05:06 -0400

    quick fix

--- Scripts/patch_test.py
+++ Scripts/patch_test.py
@@ -3,13 +3,15 @@ import numpy as np
 import imageio
 
 
-#root = '/storage/data'
-#out_path = '/storage/patches/1'
+root = '/storage/data'
+out_path = '/storage/patches/1'
 
-root = '/Users/treyoehmler/dev/tumors/data/tmp'
-out_path = '/Users/treyoehmler/dev/tumors/patches/1'
 
-patches.save_training(root, 10000, 33, out_path)
+#root = '/Users/treyoehmler/dev/tumors/data/tmp'
+#out_path = '/Users/treyoehmler/dev/tumors/patches/1'
+
+#patches.save_training(root, 10, 33, out_path)
+print('hello world')
 
 '''
 x, y = patches.load_training(out_path, 33)
