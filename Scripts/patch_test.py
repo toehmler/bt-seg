fe04fd8db00c01bcae271b25f951965adf4734fb
trey <treyoehmler@gmail.com>
2019-05-06 19:40:53 -0400

    quick fix

--- Scripts/patch_test.py
+++ Scripts/patch_test.py
@@ -7,18 +7,22 @@ import imageio
 #out_path = '/storage/s12qr5ep/patches/1'
 
 
-root = '/Users/treyoehmler/dev/tumors/data/tmp'
-out_path = '/Users/treyoehmler/dev/tumors/patches/1'
+#root = '/Users/treyoehmler/dev/tumors/data/tmp'
+#out_path = '/Users/treyoehmler/dev/tumors/patches/1'
 
-#patches.save_training(root, 10000, 33, out_path)
+root = '/home/yb/soup/data'
+out_path = '/home/yb.soup/patches'
+
+patches.save_training(root, 10000, 33, out_path)
 
 
+'''
 x, y = patches.load_training(out_path, 33)
 print(y.shape)
 
 for i in range(10):
     print(y[i])
-
+'''
 
 
 
