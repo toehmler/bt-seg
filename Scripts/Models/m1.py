555fc78a3f8ad062a845ea7f5a11524ede15b498
trey <treyoehmler@gmail.com>
2019-05-08 11:49:55 -0400

    quick fix

--- Scripts/Models/m1.py
+++ Scripts/Models/m1.py
@@ -72,19 +72,19 @@ def jeb():
     single.add(Conv2D(128, (3,3), strides=(1,1), padding='valid', 
                       kernel_regularizer=l1_l2(l1=0.01, l2=0.01),
                       activation='relu'))
+    single.add(BatchNormalization())
     single.add(Dropout(0.25))
 
     single.add(Conv2D(128, (3,3), strides=(1,1), padding='valid', 
                       kernel_regularizer=l1_l2(l1=0.01, l2=0.01),
                       activation='relu'))
-    single.add(BatchNormalization())
     single.add(Dropout(0.25))
 
     single.add(Flatten())
     single.add(Dense(5, activation='softmax'))
 
-    sgd = SGD(lr=0.001, decay=0.01, momentum=0.9)
-    single.compile(loss='categorical_crossentropy', optimizer='sgd')
+#    sgd = SGD(lr=0.001, decay=0.01, momentum=0.9)
+    single.compile(loss='categorical_crossentropy', optimizer='adam')
 
     return single
 
