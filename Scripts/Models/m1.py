579109ba5373b02c55c6d248c1662e0f8ec9cedd
trey <treyoehmler@gmail.com>
2019-05-10 22:31:54 -0400

    quick fix

--- Scripts/Models/m1.py
+++ Scripts/Models/m1.py
@@ -10,28 +10,28 @@ def compile():
 
     single.add(Conv2D(64, (7,7), 
                       strides=(1,1), padding='valid', activation='relu',
-                      #kernel_regularizer=l1_l2(l1=0.01, l2=0.01), 
+                      kernel_regularizer=l1_l2(l1=0.01, l2=0.01), 
                       input_shape=(33,33,4)))
 
-#    single.add(BatchNormalization())
-#    single.add(Dropout(0.35))
+    single.add(BatchNormalization())
+    single.add(Dropout(0.5))
 
     single.add(Conv2D(128, (5,5), strides=(1,1), padding='valid', 
-#                      kernel_regularizer=l1_l2(l1=0.01, l2=0.01),
+                      kernel_regularizer=l1_l2(l1=0.01, l2=0.01),
                       activation='relu'))
-#    single.add(BatchNormalization())
-#    single.add(Dropout(0.35))
+    single.add(BatchNormalization())
+    single.add(Dropout(0.5))
 
     single.add(Conv2D(128, (5,5), strides=(1,1), padding='valid', 
-#                      kernel_regularizer=l1_l2(l1=0.01, l2=0.01),
+                      kernel_regularizer=l1_l2(l1=0.01, l2=0.01),
                       activation='relu'))
-#    single.add(BatchNormalization())
-#    single.add(Dropout(0.35))
+    single.add(BatchNormalization())
+    single.add(Dropout(0.5))
 
     single.add(Conv2D(128, (3,3), strides=(1,1), padding='valid', 
-#                      kernel_regularizer=l1_l2(l1=0.01, l2=0.01),
+                      kernel_regularizer=l1_l2(l1=0.01, l2=0.01),
                       activation='relu'))
-#    single.add(Dropout(0.25))
+    single.add(Dropout(0.25))
 
     single.add(Flatten())
     single.add(Dense(5, activation='softmax'))
